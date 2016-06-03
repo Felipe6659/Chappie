b0df64316d861a383ce85b703a1b915eb449f9cc
Felipe6659 <felipe6659@gmail.com>
2016-06-03 10:40:51 -0300

    Create Main.ino

---
+++ Main.ino
@@ -0,0 +1,28 @@
+//--------------------  MAIN  -------------------------\\
+
+#include <Servo.h>
+
+int valorServoPararado = 90; 
+
+Servo servoBracoDireito;
+Servo servoBracoEsquerdo;
+
+Servo rodaDireita;
+Servo rodaEsquerda;
+
+void setup() 
+{
+  Serial.begin(115200);
+  servoBracoEsquerdo.attach(3);
+  servoBracoDireito.attach(5);
+  Serial.println("Começando em 10 segundos: ");
+}
+
+void loop() 
+{
+  subirBraco();
+  delay(1000);
+  parar();
+  delay(3000);
+}
+
