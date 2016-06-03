6c7bd756d0d0c168f2bc95312f59ec7e957eea40
Felipe6659 <felipe6659@gmail.com>
2016-06-03 18:18:08 +0000

    Add files via upload

---
+++ Main/Braco.ino
@@ -0,0 +1,20 @@
+/*
+ # Vire o robô de frente para você: 
+ # Abra os braços... agora já sabe onde fica a direita e a esquerda
+ */
+
+void subirBraco()
+{
+  servoBracoEsquerdo.write(valorServoPararado - 35);
+  servoBracoDireito.write(valorServoPararado - 35);
+}
+
+
+void parar()
+{
+  servoBracoEsquerdo.write(valorServoPararado);
+  servoBracoDireito.write(valorServoPararado);
+}
+
+
+
